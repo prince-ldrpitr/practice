13ef7f0bc64aba7acaabea360aa9c30a71885008
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-03-11 11:20:16 +0530

    3

---
+++ practice_set1_q3.c
@@ -0,0 +1,10 @@
+/*Write a program to convert Celsius (Centigrade degrees temperature to 
+Fahrenheit).*/
+#include<stdio.h>
+int main(){
+    int c;
+    c = 5;
+    float f = ((9*c) / 5) + 32;
+    printf("celsius to feranhit :%f",f);
+    return 0;
+}
