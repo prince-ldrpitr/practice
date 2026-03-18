9503f4bee87b4abba826a23c4e5c15518e5d5600
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-03-18 19:07:06 +0530

    1

---
+++ practice_set4_q1.c
@@ -0,0 +1,11 @@
+// Write a program to print multiplication table of a given number n.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(int i=n;i<=n*10;i=i+n){
+        printf("%d\n",i);
+    }
+    return 0;
+}
