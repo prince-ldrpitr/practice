87e7cac5d05fa930c9d22b664fb03d770c29c41e
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-03-28 14:17:27 +0530

    4

---
+++ practice_set7_q4.c
@@ -0,0 +1,12 @@
+/*Repeat problem 3 for a general input provided by the user using scanf.*/
+#include<stdio.h>
+int main(){
+	int n;
+    scanf("%d",&n);
+	int arr[10];
+	for(int i=0;i<10;i++){
+		arr[i] = n * (i+1);
+	printf("the value of %d x %d = %d\n",n,i+1,arr[i]);
+}
+	return 0;
+}
