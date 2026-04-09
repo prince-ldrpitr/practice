1bfd9511b38af0cb6bb8c4d5353d9650e6e2ecbf
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-09 10:47:00 +0530

    5

---
+++ practice_set7_q5.c
@@ -0,0 +1,24 @@
+/*Write a program containing a function which reverses the array passed to it.*/
+#include<stdio.h>
+void printarray(int a[],int n){
+	for(int i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+}
+void revers(int arr[],int n){
+	int temp;
+	for(int i=0;i<n/2;i++){
+		temp = arr[i];
+		arr[i] = arr[n-i-1];
+		arr[n-i-1] = temp;
+	}
+}
+int main(){
+	int arr[] = {1,2,3,4,5,6};
+	printarray(arr,6);
+	revers(arr,6);
+	printarray(arr,6);
+	
+	return 0;
+}
